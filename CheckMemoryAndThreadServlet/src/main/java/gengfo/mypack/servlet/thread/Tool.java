423798a199d7b28a8122baf4a2569cb93cb9b420
Forest Geng <gengfo@gmail.com>
2012-11-29 22:40:31 +0800

    Update

---
+++ CheckMemoryAndThreadServlet/src/main/java/gengfo/mypack/servlet/thread/Tool.java
@@ -0,0 +1,15 @@
+package gengfo.mypack.servlet.thread;
+
+public class Tool {
+
+	private final String name ;
+
+	public Tool(String name) {
+		this.name = name;
+	}
+
+	public String toString() {
+		return "[ " + name + " ]";
+	}
+
+}
